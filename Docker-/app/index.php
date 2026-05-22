42353833e17ac1b257200f908ede88b1f34d2165
Rafael moriya <91164317+rafael-moriya@users.noreply.github.com>
2026-05-21 22:35:47 -0300

    Add files via upload
    opa

---
+++ Docker-/app/index.php
@@ -0,0 +1,12 @@
+<?php
+echo "Docker funcionando";
+$host = 'db';
+$user = 'root';
+$pass = '1234';
+$db = 'meubanco';
+$conn = new mysqli($host, $user, $pass, $db);
+if ($conn->connect_error) {
+die("Erro: " . $conn->connect_error);
+}
+echo "Conectado ao banco!";
+?>
